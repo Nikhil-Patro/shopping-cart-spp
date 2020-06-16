52b0ef755f4f98648c0bbcf84fd356f5368b0843
Nikhil patro <nikhilpatro03@gmail.com>
2020-06-16 19:09:59 +0530

    Finally done

--- store.js
+++ store.js
@@ -33,45 +33,82 @@ let button3 = document.getElementById("3s");
 let button4 = document.getElementById("4s");
 let button5 = document.getElementById("5s");
  
-function checkShopEmpty(){      // this function basically gives a "sold out!" message if all items are selected
+function checkShopEmpty(){
     if(shop_array[0] == null){
-        let left_container  = document.getElementsByClassName("left-container");
-        let heading = document.createElement("h4");
-        heading.innerText = "We're sold out fellas!";
-        heading.style.textAlign="center"; 
-        left_container[0].appendChild(heading);
+        let right_container  = document.getElementById("shop_empty_warning");
+        right_container.style.display = "inline";
+    }
+    else {
+        let heading  = document.getElementById("shop_empty_warning");
+        heading.style.display = "none";
+        }
     }
 
-}
+function checkCartEmpty(){
+    if(cart_array[0] == null){
+        let right_container  = document.getElementById("cart_empty_warning");
+        right_container.style.display = "inline";
+    }
+    else {
+        let heading  = document.getElementById("cart_empty_warning");
+        heading.style.display = "none";
+        }
+    }
 
+
+ 
+ 
 function addToCart(){
-    console.log(event.target);
+    //console.log(event.target);
     let selected_button_id = event.target.id;
     res_id = selected_button_id.slice(0, 1) ;
     res_id = parseInt(res_id);
-    event.target.className="btn btn-danger center";
-    event.target.innerText="Remove";
-    let cart_right_container = document.getElementsByClassName("right-container");
-    let selected_item_div = event.target.parentElement.parentElement.parentElement
-    cart_right_container[0].appendChild(selected_item_div);
-    let b  = document.createElement("br");
-    selected_item_div.insertAdjacentElement("afterend", b);
-    //shop_array = shop_array.filter(function(item, selected_button_id){
-    //    return item.id != res_id;
-    //});
-    let updated_shop_array = [];
-    for(let i = 0; i < shop_array.length; i++){
-        if(shop_array[i].id != res_id){
-            updated_shop_array.push(shop_array[i]);
+    if(event.target.className == "btn btn-success center")
+    {
+        event.target.className="btn btn-danger center";
+        event.target.innerText="Remove";
+        let cart_right_container = document.getElementsByClassName("right-container");
+        let selected_item_div = event.target.parentElement.parentElement.parentElement;
+        cart_right_container[0].appendChild(selected_item_div);
+
+        let updated_shop_array = [];
+        for(let i = 0; i < shop_array.length; i++){
+            if(shop_array[i].id != res_id){
+                updated_shop_array.push(shop_array[i]);
+            }
+            else{
+                cart_array.push(shop_array[i]);
+            }
         }
-        else{
-            cart_array.push(shop_array[i]);
-        }
+        shop_array = updated_shop_array;
+        console.log(shop_array);
+        console.log(cart_array);
+        checkShopEmpty();
+        checkCartEmpty(); 
     }
-    shop_array = updated_shop_array;
-    console.log(shop_array);
-    console.log(cart_array);
-    checkShopEmpty(); 
+    else
+    {   
+        event.target.className="btn btn-success center";
+        event.target.innerText="Add to Cart";
+        let cart_left_container = document.getElementsByClassName("left-container");
+        let selected_item_div = event.target.parentElement.parentElement.parentElement;
+        cart_left_container[0].appendChild(selected_item_div);
+        let updated_cart_array = [];
+        for(let i = 0; i < cart_array.length; i++){
+            if(cart_array[i].id != res_id){
+                updated_cart_array.push(cart_array[i]);
+            }
+            else{
+                shop_array.push(cart_array[i]);
+            }
+        }
+        cart_array = updated_cart_array;
+        console.log(shop_array);
+        console.log(cart_array);
+        checkCartEmpty(); 
+        checkShopEmpty();
+
+    }
 }
 button1.addEventListener("click", addToCart);
 button2.addEventListener("click", addToCart);
