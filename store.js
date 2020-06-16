b1870cfa2308c6f3081f5744a9b352e630548a88
Nikhil patro <nikhilpatro03@gmail.com>
2020-06-16 10:56:24 +0530

    javascript showing error

---
+++ store.js
@@ -0,0 +1,57 @@
+let all_items_array =[
+    {
+        id : 1,
+        name : boots,
+        price : 1000,
+        inCart : false,
+        inShop : true,
+    },
+    {
+        id : 2,
+        name : mask,
+        price : 50,
+        inCart : false,
+        inShop : true,
+    },
+    {
+        id : 3,
+        name : raincoat,
+        price : 1000,
+        inCart : false,
+        inShop : true,
+    },
+    {
+        id : 4,
+        name : sweatshirt,
+        price : 2000,
+        inCart : false,
+        inShop : true,
+    },
+    {
+        id : 5,
+        name : umbrella,
+        price : 350,
+        inCart : false,
+        inShop : true,
+    }];
+let button1 = document.getElementById("1s");
+let button2 = document.getElementById("2s");
+let button3 = document.getElementById("3s");
+let button4 = document.getElementById("4s");
+let button5 = document.getElementById("5s");
+button1.addEventListener("click", function(){
+    console.log("Hi");
+});
+button2.addEventListener("click", function(){
+    console.log("Hi");
+});
+button3.addEventListener("click", function(){
+    console.log("Hi");
+});
+button4.addEventListener("click", function(){
+    console.log("Hi");
+});
+button5.addEventListener("click", function(){
+    console.log("Hi");
+});
+ 
